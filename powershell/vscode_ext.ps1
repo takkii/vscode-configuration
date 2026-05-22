3d2f1da8a147df81a53ff3a9b6c392c9af527e06
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-05-22 17:07:57 +0900

    Update.

--- powershell/vscode_ext.ps1
+++ powershell/vscode_ext.ps1
@@ -2,13 +2,13 @@ code --install-extension aaron-bond.better-comments
 code --install-extension batisteo.vscode-django
 code --install-extension castwide.solargraph
 code --install-extension christian-kohler.path-intellisense
+code --install-extension denoland.vscode-deno
 code --install-extension ecmel.vscode-html-css
 code --install-extension eeyore.yapf
 code --install-extension enkia.tokyo-night
 code --install-extension eservice-online.vs-sharper
 code --install-extension formulahendry.code-runner
 code --install-extension formulahendry.dotnet-test-explorer
-code --install-extension github.copilot-chat
 code --install-extension jakebecker.elixir-ls
 code --install-extension jesschadwick.nuget-reverse-package-search
 code --install-extension jinxdash.prettier-rust
